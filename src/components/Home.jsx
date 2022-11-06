bd2fe6f6319855c6ee358ce721f817b8113ca4c7
JioR95 <jiovani.rivera@rocketmail.com>
2022-11-05 20:15:19 -0400

    nav bar update to components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-full h-full bg-[#0a192f]'>
+    <div className='w-full h-screen bg-[rgb(3,2,2)]'>
     
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React,{useState} from 'react'
-import {FaBars, FaTimes, FaFacebook } from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import Logo from '../assets/My logo-1.png'
 
 const NavBar = () => {
     const [nav, setNav]=useState(false)
     const handleclick = () => setNav(!nav)
   return (
-    <div className='fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#070808] text-red-600 '>
+    <div className='fixed w-full h-[100px] flex justify-between items-center px-3 bg-[#040404] text-red-600 '>
         <div>
             <img src={Logo} alt="Logo" style={{ width: '100px'}} />
         </div>
@@ -30,7 +30,7 @@ const NavBar = () => {
         {/*mobilemenu*/}
         
        
-        <ul className={!nav ?'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <ul className={!nav ?'hidden':'absolute top-0 left-0 w-full h-screen bg-[#090909] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl '>Home</li>
                 <li className='py-6 text-4xl '>About Me</li>
                 <li className='py-6 text-4xl '>Portfolio</li>
@@ -39,11 +39,24 @@ const NavBar = () => {
 
             {/*socialicons*/}
 
-            <div className='flex fixed flex-col top-[35%] left-0'>
+            <div className='hidden md:flex fixed flex-col bottom-[35%] left-0'>
                 <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center '>
-                        <a className ='flex justify-between items-center'href='/'>
-                            Linkedin <FaFacebook size={30}/>
+                    <li className='w-[175px] h-[100px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] bg-[#c00c0c] '>
+                        <a className ='flex justify-between items-center w-full text-gray-200'
+                            href='/'>
+                            Linkedin <FaLinkedin size={30}/>
+                        </a>
+                    </li>
+                    <li className='w-[175px] h-[100px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] bg-[#5e5e7077] '>
+                        <a className ='flex justify-between items-center w-full text-gray-200'
+                            href='/'>
+                            Github <FaGithub size={30}/>
+                        </a>
+                    </li>
+                    <li className='w-[175px] h-[100px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] '>
+                        <a className ='flex justify-between items-center w-full text-gray-200'
+                            href='/'>
+                             <FaLinkedin size={30}/>
                         </a>
                     </li>
                 </ul>
